486e855a8e5190b2ae8fed9a50b5ec6f680dfea6
HomeACcessoryKid <homeaccessorykid@gmail.com>
2024-07-28 11:09:41 +0200

    0.0.6 split report processes

--- main/main.c
+++ main/main.c
@@ -25,56 +25,94 @@
 
 #define ESP_INTR_FLAG_DEFAULT 0
 
-static QueueHandle_t gpio_evt_queue;
+static QueueHandle_t gpio_evt_queue0;
+static QueueHandle_t gpio_evt_queue1;
 
 #define  READY 0
 #define  START 1
 #define  RECV  2
-int      resp_idx=0, rx_state=READY;
-uint32_t response=0;
+int      resp_idx0=0, rx_state0=READY;
+int      resp_idx1=0, rx_state1=READY;
+uint32_t response0=0;
+uint32_t response1=0;
 static void IRAM_ATTR gpio_isr_handler(void* arg) {
     uint32_t gpio_num = (uint32_t) arg;
-    static uint64_t before=0;
+    static uint64_t before0=0;
+    static uint64_t before1=0;
     uint64_t now,delta;
     int even,inv_read;
     switch (gpio_num) {
         case GPIO_INPUT_IO_0: {
+            now=esp_timer_get_time(),delta=now-before0;
+            even=0, inv_read=gpio_get_level(GPIO_INPUT_IO_0);//note that gpio_read gives the inverted value of the symbol
+            if (rx_state0==READY) {
+                if (inv_read) return;
+                rx_state0=START;
+                before0=now;
+            } else if (rx_state0==START) {
+                if (400<delta && delta<650 && inv_read) {
+                    resp_idx0=0; response0=0; even=0;
+                    rx_state0=RECV;
+                } //else error state but might be a new start, so just stay in this state
+                before0=now;
+            } else if (rx_state0==RECV)  {
+                if (900<delta && delta<1150) {
+                    if (resp_idx0<32) {
+                        response0=(response0<<1)|inv_read;
+                        if (inv_read) even++;
+                        resp_idx0++;
+                        before0=now;
+                    } else {
+                        if (even%2==0) {
+                            if (response0&0x0f000000) resp_idx0=-2; //signal issue reserved bits not zero
+                            else {
+                                response0&=0x7fffffff; //mask parity bit
+                                xQueueSendToBackFromISR(gpio_evt_queue0, (void*)&response0, NULL);
+                            }
+                        } else resp_idx0=-1; //signal issue parity failure
+                        rx_state0=READY;
+                    }
+                } else if (delta>=1150) { //error state
+                    if (inv_read) rx_state0=READY;
+                    else {rx_state0=START; before0=now;}
+                } //else do nothing so before0+=500 and next transit is a databit
+            }
             
             break;}
         case GPIO_INPUT_IO_1: {
-            now=esp_timer_get_time(),delta=now-before;
+            now=esp_timer_get_time(),delta=now-before1;
             even=0, inv_read=gpio_get_level(GPIO_INPUT_IO_1);//note that gpio_read gives the inverted value of the symbol
-            if (rx_state==READY) {
+            if (rx_state1==READY) {
                 if (inv_read) return;
-                rx_state=START;
-                before=now;
-            } else if (rx_state==START) {
+                rx_state1=START;
+                before1=now;
+            } else if (rx_state1==START) {
                 if (400<delta && delta<650 && inv_read) {
-                    resp_idx=0; response=0; even=0;
-                    rx_state=RECV;
+                    resp_idx1=0; response1=0; even=0;
+                    rx_state1=RECV;
                 } //else error state but might be a new start, so just stay in this state
-                before=now;
-            } else if (rx_state==RECV)  {
+                before1=now;
+            } else if (rx_state1==RECV)  {
                 if (900<delta && delta<1150) {
-                    if (resp_idx<32) {
-                        response=(response<<1)|inv_read;
+                    if (resp_idx1<32) {
+                        response1=(response1<<1)|inv_read;
                         if (inv_read) even++;
-                        resp_idx++;
-                        before=now;
+                        resp_idx1++;
+                        before1=now;
                     } else {
                         if (even%2==0) {
-                            if (response&0x0f000000) resp_idx=-2; //signal issue reserved bits not zero
+                            if (response1&0x0f000000) resp_idx1=-2; //signal issue reserved bits not zero
                             else {
-                                response&=0x7fffffff; //mask parity bit
-                                xQueueSendToBackFromISR(gpio_evt_queue, (void*)&response, NULL);
+                                response1&=0x7fffffff; //mask parity bit
+                                xQueueSendToBackFromISR(gpio_evt_queue1, (void*)&response1, NULL);
                             }
-                        } else resp_idx=-1; //signal issue parity failure
-                        rx_state=READY;
+                        } else resp_idx1=-1; //signal issue parity failure
+                        rx_state1=READY;
                     }
                 } else if (delta>=1150) { //error state
-                    if (inv_read) rx_state=READY;
-                    else {rx_state=START; before=now;}
-                } //else do nothing so before+=500 and next transit is a databit
+                    if (inv_read) rx_state1=READY;
+                    else {rx_state1=START; before1=now;}
+                } //else do nothing so before1+=500 and next transit is a databit
             }
             break;}
         default:
@@ -82,9 +120,34 @@ static void IRAM_ATTR gpio_isr_handler(void* arg) {
     }
 }
 
+void task0(void *arg) {
+    uint32_t message;
+    while (true) {
+        if (xQueueReceive(gpio_evt_queue0, &(message), (TickType_t)850/portTICK_PERIOD_MS) == pdTRUE) {
+            UDPLUS("Slave: %08lx\n",message);
+        } else {
+            UDPLUS("!!! NO_RSP Slave:  resp_idx=%d rx_state=%d response=%08lx\n",resp_idx0, rx_state0, response0);
+            resp_idx0=0, rx_state0=READY, response0=0;
+        }
+    }
+}
+
+void task1(void *arg) {
+    uint32_t message;
+    while (true) {
+        if (xQueueReceive(gpio_evt_queue1, &(message), (TickType_t)850/portTICK_PERIOD_MS) == pdTRUE) {
+            UDPLUS("Master:%08lx\n",message);
+        } else {
+            UDPLUS("!!! NO_RSP Master: resp_idx=%d rx_state=%d response=%08lx\n",resp_idx1, rx_state1, response1);
+            resp_idx1=0, rx_state1=READY, response1=0;
+        }
+    }
+}
+
 void main_task(void *arg) {
     udplog_init(3);
-    gpio_evt_queue = xQueueCreate(1000, sizeof(uint32_t));
+    gpio_evt_queue0 = xQueueCreate(10, sizeof(uint32_t));
+    gpio_evt_queue1 = xQueueCreate(10, sizeof(uint32_t));
         
     gpio_config_t io_conf = {}; //zero-initialize the config structure.
 
@@ -104,22 +167,17 @@ void main_task(void *arg) {
 
    
     gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT); //install gpio isr service
-//     gpio_isr_handler_add(GPIO_INPUT_IO_0, gpio_isr_handler, (void*) GPIO_INPUT_IO_0); //hook isr handler for specific gpio pin
+    gpio_isr_handler_add(GPIO_INPUT_IO_0, gpio_isr_handler, (void*) GPIO_INPUT_IO_0); //hook isr handler for specific gpio pin
     gpio_isr_handler_add(GPIO_INPUT_IO_1, gpio_isr_handler, (void*) GPIO_INPUT_IO_1); //hook isr handler for specific gpio pin
     
     gpio_dump_io_configuration(stdout, (GPIO_INPUT_PIN_SEL|GPIO_OUTPUT_PIN_SEL) );
     
-    uint32_t message;
-    while (true) {
-        if (xQueueReceive(gpio_evt_queue, &(message), (TickType_t)850/portTICK_PERIOD_MS) == pdTRUE) {
-            UDPLUS("RSP:%08lx\n",message);
-        } else {
-            UDPLUS("!!! NO_RSP: resp_idx=%d rx_state=%d response=%08lx\n",resp_idx, rx_state, response);
-            resp_idx=0, rx_state=READY, response=0;
-        }
+    xTaskCreate(task0,"task0",4096,NULL,1,NULL);
+    xTaskCreate(task1,"task1",4096,NULL,1,NULL);
+    while (true) { //TODO: if task deleted, semaphore of UDPlogger also 
+        vTaskDelay(1000); 
     }
-    
-
+    vTaskDelete(NULL);
 }    
 
 void app_main(void) {
